30eb73253f9749d13598cf56a71d021059b0ceef
Solangeilinga <solangeilinga@gmail.com>
2026-07-05 18:29:46 +0000

    security

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,14 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
 });
+
+// Désactiver les prepared statements pour pgbouncer
+prisma.$connect();
 
 module.exports = prisma;
